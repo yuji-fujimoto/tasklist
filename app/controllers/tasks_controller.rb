885d63f158e70f7e7e363fdea6ede4dab75b2157
yuji.fujimoto <dir-zan@hotmail.co.jp>
2020-07-25 03:34:51 +0000

    login function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,11 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = Task.all
+    user = @current_user
+    @tasks = user.tasks
   end
   
   def show
@@ -15,6 +17,7 @@ class TasksController < ApplicationController
   
   def create
     @task = Task.new(task_params)
+    @task.user = current_user
     
     if @task.save
       flash[:success] = 'Task が正常に保存されました'
@@ -46,13 +49,21 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
   
+  
   private
   
   def set_task
     @task = Task.find(params[:id])
   end
-  #Strong Parameter
+  
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content, :status, :user_id)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
+      flash[:success] = 'ユーザを登録しました。'
       redirect_to '/tasks'
     else
+      flash[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
